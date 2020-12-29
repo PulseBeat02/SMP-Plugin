b774a55178ddb3f0a8a2708874a44f74341babc7
Brandon Li <40838203+PulseBeat02@users.noreply.github.com>
2020-12-29 13:18:09 -0500

    Added Console Command

--- src/main/java/com/github/pulsebeat02/CommandListener.java
+++ src/main/java/com/github/pulsebeat02/CommandListener.java
@@ -1,9 +1,11 @@
 package com.github.pulsebeat02;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 
@@ -25,59 +27,74 @@ public class CommandListener implements CommandExecutor, TabCompleter {
             sender.sendMessage(plugin.formatMessage(ChatColor.RED + "You must be a Player to execute this command!"));
             return true;
         }
-        Player pl = (Player) sender;
-        UUID player = pl.getUniqueId();
-        PlayerStatus status = plugin.getStatus().get(player);
-        if (args.length == 0) {
-            sender.sendMessage(plugin.formatMessage(ChatColor.GOLD + "Current Status: " + (status.isWar() ? "War" : "Peaceful")));
-            return true;
-        }
-        if (args.length != 2) {
-            sender.sendMessage(plugin.formatMessage(ChatColor.RED + "Invalid arguments. /status set [peaceful | war]"));
-            return true;
-        }
-        if (!args[0].equalsIgnoreCase("set")) {
-            sender.sendMessage(plugin.formatMessage(ChatColor.RED + "Invalid arguments. /status set [mode]"));
-            return true;
-        }
-        if (status.isCombat()) {
-            sender.sendMessage(plugin.formatMessage(ChatColor.RED + "You cannot execute this command while combat tagged!"));
-            return true;
-        }
-        switch (args[1]) {
-            case "peaceful":
-                if (status.isWar()) {
-                    long time = status.getPeacefulCooldown();
-                    if (time <= 0) {
-                        plugin.getStatus().get(player).setWar(false);
-                        plugin.getStatus().get(player).setPeacefulCooldown(GlobalTime.PEACEFUL_TO_WAR.getTime());
-                        plugin.getPeaceful().addEntry(pl.getName());
-                        pl.sendMessage(plugin.formatMessage(ChatColor.GREEN + "Successfully switched to Peaceful Mode"));
+        if (sender instanceof ConsoleCommandSender) {
+            if (args.length != 3 || !args[0].equalsIgnoreCase("set")) {
+                sender.sendMessage(plugin.formatMessage(ChatColor.RED + "Invalid arguments. /status set [username] [minutes]"));
+                return true;
+            }
+            Player pl = Bukkit.getPlayer(args[1]);
+            if (pl == null) {
+                sender.sendMessage(plugin.formatMessage(ChatColor.RED + "Player Username not Found!"));
+                return true;
+            }
+            int minutes;
+            try {
+                minutes = Integer.parseInt(args[2]);
+                plugin.getStatus().get(pl.getUniqueId()).setCombatCooldown(minutes);
+            } catch (NumberFormatException e) {
+                sender.sendMessage(plugin.formatMessage(ChatColor.RED + "Minutes must be an Integer!"));
+            }
+        } else {
+            Player pl = (Player) sender;
+            UUID player = pl.getUniqueId();
+            PlayerStatus status = plugin.getStatus().get(player);
+            if (args.length == 0) {
+                sender.sendMessage(plugin.formatMessage(ChatColor.GOLD + "Current Status: " + (status.isWar() ? "War" : "Peaceful")));
+                return true;
+            }
+            if (args.length != 2 || !args[0].equalsIgnoreCase("set")) {
+                sender.sendMessage(plugin.formatMessage(ChatColor.RED + "Invalid arguments. /status set [peaceful | war]"));
+                return true;
+            }
+            if (status.isCombat()) {
+                sender.sendMessage(plugin.formatMessage(ChatColor.RED + "You cannot execute this command while combat tagged!"));
+                return true;
+            }
+            switch (args[1]) {
+                case "peaceful":
+                    if (status.isWar()) {
+                        long time = status.getPeacefulCooldown();
+                        if (time <= 0) {
+                            plugin.getStatus().get(player).setWar(false);
+                            plugin.getStatus().get(player).setPeacefulCooldown(GlobalTime.PEACEFUL_TO_WAR.getTime());
+                            plugin.getPeaceful().addEntry(pl.getName());
+                            pl.sendMessage(plugin.formatMessage(ChatColor.GREEN + "Successfully switched to Peaceful Mode"));
+                        } else {
+                            pl.sendMessage(plugin.formatMessage(ChatColor.RED + "You still have " + time / 1200 + " seconds before switching"));
+                        }
                     } else {
-                        pl.sendMessage(plugin.formatMessage(ChatColor.RED + "You still have " + time/1200 + " seconds before switching"));
+                        pl.sendMessage(plugin.formatMessage(ChatColor.GOLD + "You are Already in Peaceful Mode!"));
                     }
-                } else {
-                    pl.sendMessage(plugin.formatMessage(ChatColor.GOLD + "You are Already in Peaceful Mode!"));
-                }
-                break;
-            case "war":
-                if (!status.isWar()) {
-                    long time = status.getWarCooldown();
-                    if (time <= 0) {
-                        plugin.getStatus().get(player).setWar(true);
-                        plugin.getStatus().get(player).setWarCooldown(GlobalTime.WAR_TO_PEACEFUL.getTime());
-                        plugin.getWar().addEntry(pl.getName());
-                        pl.sendMessage(plugin.formatMessage(ChatColor.GREEN + "Successfully switched to War Mode"));
+                    break;
+                case "war":
+                    if (!status.isWar()) {
+                        long time = status.getWarCooldown();
+                        if (time <= 0) {
+                            plugin.getStatus().get(player).setWar(true);
+                            plugin.getStatus().get(player).setWarCooldown(GlobalTime.WAR_TO_PEACEFUL.getTime());
+                            plugin.getWar().addEntry(pl.getName());
+                            pl.sendMessage(plugin.formatMessage(ChatColor.GREEN + "Successfully switched to War Mode"));
+                        } else {
+                            pl.sendMessage(plugin.formatMessage(ChatColor.RED + "You still have " + time / 1200 + " seconds before switching"));
+                        }
                     } else {
-                        pl.sendMessage(plugin.formatMessage(ChatColor.RED + "You still have " + time/1200 + " seconds before switching"));
+                        pl.sendMessage(plugin.formatMessage(ChatColor.GOLD + "You are Already in War Mode!"));
                     }
-                } else {
-                    pl.sendMessage(plugin.formatMessage(ChatColor.GOLD + "You are Already in War Mode!"));
-                }
-                break;
-            default:
-                pl.sendMessage(plugin.formatMessage(ChatColor.RED + "Bruh that isn't a valid argument"));
-                break;
+                    break;
+                default:
+                    pl.sendMessage(plugin.formatMessage(ChatColor.RED + "Bruh that isn't a valid argument"));
+                    break;
+            }
         }
         return true;
     }
