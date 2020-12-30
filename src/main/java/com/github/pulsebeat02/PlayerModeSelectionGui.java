7f3ac7fb23474aca21f92ca8553f087e466328b7
Brandon Li <40838203+PulseBeat02@users.noreply.github.com>
2020-12-30 14:44:45 -0500

    t

---
+++ src/main/java/com/github/pulsebeat02/PlayerModeSelectionGui.java
@@ -0,0 +1,75 @@
+package com.github.pulsebeat02;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.HumanEntity;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.UUID;
+
+public class PlayerModeSelectionGui implements Listener {
+
+    private final SMPPlugin plugin;
+    private final UUID who;
+    private final Inventory inv;
+
+    public PlayerModeSelectionGui(final SMPPlugin plugin, final UUID who) {
+        plugin.getServer().getPluginManager().registerEvents(this, plugin);
+        this.plugin = plugin;
+        this.who = who;
+        this.inv = Bukkit.createInventory(null, 3, "Choose Mode");
+        inv.setItem(13, getPeacefulStack());
+        inv.setItem(15, getWarStack());
+    }
+
+    private ItemStack getPeacefulStack() {
+        ItemStack stack = new ItemStack(Material.LIME_STAINED_GLASS_PANE);
+        ItemMeta meta = stack.getItemMeta();
+        meta.setDisplayName(ChatColor.GREEN + " " + ChatColor.BOLD + "Peaceful");
+        meta.setLore(Collections.singletonList(ChatColor.GOLD + "Choose me if you want to be PEACEFUL, and not Fight!"));
+        meta.addEnchant(Enchantment.DURABILITY, 1, false);
+        meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+        stack.setItemMeta(meta);
+        return stack;
+    }
+
+    private ItemStack getWarStack() {
+        ItemStack stack = new ItemStack(Material.RED_STAINED_GLASS_PANE);
+        ItemMeta meta = stack.getItemMeta();
+        meta.setDisplayName(ChatColor.RED + " " + ChatColor.BOLD + "War");
+        meta.setLore(Arrays.asList(ChatColor.GOLD + "Choose me if you want to FIGHT!", ChatColor.RED + "" + ChatColor.ITALIC + "Blood for the Blood King"));
+        meta.addEnchant(Enchantment.DURABILITY, 1, false);
+        meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+        stack.setItemMeta(meta);
+        return stack;
+    }
+
+    @EventHandler
+    public void onInventoryClick(final InventoryClickEvent event) {
+        UUID ent = event.getWhoClicked().getUniqueId();
+        if (ent != who) {
+            return;
+        }
+        if (event.getClickedInventory().hashCode() != inv.hashCode()) {
+            return;
+        }
+        switch (event.getSlot()) {
+            case 13:
+                plugin.getStatus().put(ent, new PlayerStatus( false, GlobalTime.WAR_TO_PEACEFUL.getTime(), GlobalTime.PEACEFUL_TO_WAR.getTime(), false, 0));
+                break;
+
+        }
+    }
+
+
+}
