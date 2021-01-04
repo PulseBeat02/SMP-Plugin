76a10f9594858bde17658cd984deef5e90ab33d1
Brandon Li <40838203+PulseBeat02@users.noreply.github.com>
2021-01-03 19:52:20 -0500

    Fixed ConcurrentModificationException

--- src/main/java/com/github/pulsebeat02/listener/PlayerServerLeaveListener.java
+++ src/main/java/com/github/pulsebeat02/listener/PlayerServerLeaveListener.java
@@ -1,13 +1,10 @@
 package com.github.pulsebeat02.listener;
 
 import com.github.pulsebeat02.SMPPlugin;
-import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
-
-import java.util.Map;
 
 public class PlayerServerLeaveListener implements Listener {
 
@@ -20,11 +17,7 @@ public class PlayerServerLeaveListener implements Listener {
     @EventHandler
     public void onPlayerLeave(final PlayerQuitEvent event) {
         Player player = event.getPlayer();
-        for (Map.Entry<CommandSender, CommandSender> msg : plugin.getMessages().entrySet()) {
-            if (msg.getKey().equals(player) || msg.getValue().equals(player)) {
-                plugin.getMessages().remove(msg.getKey());
-            }
-        }
+        plugin.getMessages().entrySet().removeIf(entry -> entry.getKey().equals(player) || entry.getValue().equals(player));
     }
 
 }
